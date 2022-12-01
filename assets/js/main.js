8c79aa4ecfcda6cefdd794c5d85cc2a4b6d61405
daniell wendyson <daniellwendyson@gmail.com>
2022-12-01 12:20:04 -0300

    contato js

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,3 +17,46 @@ function activeWork() {
 }
 
 linkWork.forEach((a) => a.addEventListener('click', activeWork));
+
+/* contato */
+const contactForm = document.getElementById('contact-form'),
+contactName = document.getElementById('contact-name'),
+contactEmail = document.getElementById('contact-email'),
+Message = document.getElementById('message'),
+contactMessage = document.getElementById('contact-message');
+
+const sendEmail = (e) => {
+    e.preventDefault();
+
+    // checar se o campo tem algum valor
+    if(contactName.value === '' || contactEmail.value === '' || Message.value === '') {
+        // adicionar e remover cor
+        contactMessage.classList.remove('color-light');
+        contactMessage.classList.add('color-dark');
+
+        // mostrar mensagem
+        contactMessage.textContent = 'Preencha todos os campos!';
+    }else {
+        // serviceID - templateID - #form - publickey
+        emailjs.sendForm('service_9pqh7o1', 'template_n3qwvkr', '#contact-form', 'GxGD_QQrZSRiLUou7')
+        .then(() => {
+            // mostrar mensagem e adicionar cor
+            contactMessage.classList.add('color-light');
+            contactMessage.textContent = 'Mensagem enviada!';
+
+            // remover mensagem depois de 5 segundos
+            setTimeout(() => {
+                contactMessage.textContent = '';
+            }, 5000) 
+        }, (error) => {
+            alert('OOPs! ALGO DEU ERRADO...', error)
+        });
+
+        // limpar campos
+        contactName.value = '';
+        contactEmail.value = '';
+        Message.value = '';
+    }
+};
+
+contactForm.addEventListener('submit', sendEmail);
